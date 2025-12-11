5f5f66898ced627965fc5657bf588508e8becee9
Michele <mikelemigliore@hotmail.com>
2025-12-10 22:20:50 -0500

    Last Commit

--- lib/db.ts
+++ lib/db.ts
@@ -15,17 +15,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-// Use env var if present; otherwise fall back to your RDS URL
-const DATABASE_URL =
-  process.env.DATABASE_URL ??
-  "postgresql://postgres:Juventus122510@database-1.cbkgay80ag2k.us-east-2.rds.amazonaws.com:5432/database-1?schema=public";
-
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     datasources: {
       db: {
-        url: DATABASE_URL,
+        url: process.env.DATABASE_URL,
       },
     },
   });
